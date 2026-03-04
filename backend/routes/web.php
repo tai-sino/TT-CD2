2e8c5d6623309e62c2c3355482a829859c8a47ec
khanh779-9 <khanh779-9@users.noreply.github.com>
2026-03-04 22:06:40 +0700

    fix some issue

---
+++ backend/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Backend is running',
+        'api' => url('/api'),
+        'health' => url('/up'),
+    ]);
+});
