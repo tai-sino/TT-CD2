dfc3f8f1bf9a056c18e523131ca53b6b2b3d44a7
tai-sino <taisino291@gmail.com>
2026-04-07 22:15:06 +0700

    feat(02-01): them SinhVienController import Excel + CRUD va routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SinhVienController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/students/import', [SinhVienController::class, 'import']);
+    Route::get('/students', [SinhVienController::class, 'index']);
+    Route::post('/students', [SinhVienController::class, 'store']);
+    Route::put('/students/{mssv}', [SinhVienController::class, 'update']);
+    Route::delete('/students/{mssv}', [SinhVienController::class, 'destroy']);
 });
